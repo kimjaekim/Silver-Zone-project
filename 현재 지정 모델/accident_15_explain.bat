2acc605bf66662758f62c81f4de5a389733f919b
kimjaekim <144304318+kimjaekim@users.noreply.github.com>
2025-10-22 15:40:30 +0900

    Add files via upload

---
+++ 현재 지정 모델/accident_15_explain.bat
@@ -0,0 +1,2 @@
+java -mx500m -cp "/C:/Users/%ec%9c%a4%ed%99%8d%ec%8b%9d/Downloads/maxent/maxent/maxent.jar;maxent.jar" density.Explain -l "C:\Users\윤홍식\Desktop\동대문구 공모전\현재 지정 모델\accident_15.lambdas" -c "C:\Users\윤홍식\Desktop\동대문구 공모전\현재 지정 모델\accident_15.asc" "C:\Users\윤홍식\Desktop\동대문구 공모전\환경변수"
+@if errorlevel 1 pause
