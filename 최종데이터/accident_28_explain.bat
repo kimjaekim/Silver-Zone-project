32b95768b64b430d325a578249b17e3150dc9450
kimjaekim <144304318+kimjaekim@users.noreply.github.com>
2025-10-22 14:23:15 +0900

    Add files via upload

---
+++ 최종데이터/accident_28_explain.bat
@@ -0,0 +1,2 @@
+java -mx500m -cp "/C:/Users/%ec%9c%a4%ed%99%8d%ec%8b%9d/Downloads/maxent/maxent/maxent.jar;maxent.jar" density.Explain -l "C:\Users\윤홍식\Desktop\동대문구 공모전\재현이 실수해서 다시한거\accident_28.lambdas" -c "C:\Users\윤홍식\Desktop\동대문구 공모전\재현이 실수해서 다시한거\accident_28.asc" "C:\Users\윤홍식\Desktop\동대문구 공모전\환경변수"
+@if errorlevel 1 pause
